4b6b285a1a52783773b787dc2cfa5f5bc7d12e6e
Thomas Hybel <tehybel@gmail.com>
2023-05-12 18:24:54 +0200

    changed command registration to be global instead of guild-specific

--- src/discord/deployCommands.ts
+++ src/discord/deployCommands.ts
@@ -2,24 +2,29 @@
 import { REST, Routes, SlashCommandBuilder } from "discord.js";
 import dotenv = require("dotenv");
 import SlashCommandName from "./SlashCommandName";
-dotenv.config();
 
 // This file should be run every time the definitions of the slash commands change
 // `npm run discord`
 
-const deployDev = false;
 const deployProd = false;
 
 let BOT_TOKEN: string;
 let APPLICATION_ID: string;
 
-if (deployDev) {
-    BOT_TOKEN = process.env.DISCORD_BOT_TOKEN!;
-    APPLICATION_ID = "761897641591701524";
+const parsed_config = dotenv.config();
+if (parsed_config.error) {
+    throw parsed_config.error;
 }
+
+if (process.env.DISCORD_BOT_CLIENTID === undefined) {
+	console.log("DISCORD_BOT_CLIENTID is undefined, cannot continue.");
+	process.exit(-1);
+}
+
+APPLICATION_ID = "761897641591701524";
+BOT_TOKEN = process.env.DISCORD_BOT_TOKEN!;
 if (deployProd) {
     BOT_TOKEN = process.env.DISCORD_BOT_TOKEN_PROD!;
-    APPLICATION_ID = "1089945324757454950";
 }
 
 // Construct and prepare an instance of the REST module
@@ -49,7 +54,7 @@ const allCommands = [
 
         // The put method is used to fully refresh all commands in the guild with the current set
         const data = await rest.put(
-            Routes.applicationCommands(APPLICATION_ID!),
+            Routes.applicationCommands(process.env.DISCORD_BOT_CLIENTID!),
             {
                 body: allCommands.map((cmd) => cmd.toJSON()),
             }
